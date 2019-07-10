bda1c59f47c39697c5a287f63410e66fa3047059
Rafa Mel <melrcm@gmx.net>
2019-07-10 14:47:12 +0200

    feat: adds pipe

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,2 @@
+export { default as pipe } from './pipe';
 export * from './types';
---
+++ src/pipe.ts
@@ -0,0 +1,36 @@
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+import { typedPipe } from 'ts-functional-pipe';
+import { TEmptyFuInstance, IFuInstance } from './types';
+
+const _pipe = typedPipe<TEmptyFuInstance>();
+
+function pipeT<T>() {
+  return typedPipe<Required<IFuInstance<T>>>();
+}
+
+function pipeP<T = any>() {
+  return pipeT<{ props: T }>();
+}
+
+function pipeC<T = any>() {
+  return pipeT<{ context: T }>();
+}
+
+function pipePC<P = any, C = any>() {
+  return pipeT<{ props: P; context: C }>();
+}
+
+export type Pipe = typeof _pipe & {
+  t: typeof pipeT;
+  p: typeof pipeP;
+  c: typeof pipeC;
+  pc: typeof pipePC;
+};
+
+const pipe: Pipe = _pipe as Pipe;
+pipe.t = pipeT;
+pipe.p = pipeP;
+pipe.c = pipeC;
+pipe.pc = pipePC;
+
+export default pipe;
