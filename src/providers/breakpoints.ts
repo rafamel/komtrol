49beb2cccc852549a6a227187a713df7524f9745
Rafa Mel <melrcm@gmx.net>
2019-07-11 19:30:57 +0200

    feat(withBreakpoints): takes a map function as argument

--- src/providers/breakpoints.ts
+++ src/providers/breakpoints.ts
@@ -1,6 +1,7 @@
 import { stateful } from '~/abstracts';
 import { TFu } from '~/types';
 import { mapTo } from '~/utils';
+import { shallowEqualProps as equal } from 'shallow-equal-props';
 
 export interface IBreakpoints {
   [key: string]: number | string;
@@ -18,20 +19,42 @@ function withBreakpoints<A extends object, T extends IBreakpoints>(
 function withBreakpoints<
   A extends object,
   T extends IBreakpoints,
-  K extends string
->(key: K, breakpoints: T): TFu<A, A & { [P in K]: TActiveBreakpoints<T> }>;
-
+  U extends object
+>(breakpoints: T, map: (active: TActiveBreakpoints<T>) => U): TFu<A, A & U>;
 function withBreakpoints<
   A extends object,
   T extends IBreakpoints,
   K extends string
+>(key: K, breakpoints: T): TFu<A, A & { [P in K]: TActiveBreakpoints<T> }>;
+function withBreakpoints<
+  A extends object,
+  T extends IBreakpoints,
+  U,
+  K extends string
+>(
+  key: K,
+  breakpoints: T,
+  map: (active: TActiveBreakpoints<T>) => U
+): TFu<A, A & { [P in K]: U }>;
+
+function withBreakpoints<
+  A extends object,
+  T extends IBreakpoints,
+  U,
+  K extends string
 >(
   a: K | T,
-  b?: T
-): TFu<A, A & (TActiveBreakpoints<T> | { [P in K]: TActiveBreakpoints<T> })> {
-  const key = b ? (a as K) : null;
-  const breakpoints = b || (a as T);
-  const mapper = mapTo<A, TActiveBreakpoints<T>, K>(key);
+  b?: T | ((active: TActiveBreakpoints<T>) => U),
+  c?: (active: TActiveBreakpoints<T>) => U
+): TFu<
+  A,
+  A & (TActiveBreakpoints<T> | U | { [P in K]: TActiveBreakpoints<T> | U })
+> {
+  const hasKey = typeof a === 'string';
+  const key = hasKey ? (a as K) : null;
+  const breakpoints = (hasKey ? b : a) as T;
+  const map = hasKey ? c : (b as (active: TActiveBreakpoints<T>) => U);
+  const mapper = mapTo<A, TActiveBreakpoints<T> | U, K>(key);
 
   const arr = Object.entries(breakpoints)
     .map(([name, value]): [keyof T, number] => [name, parseInt(String(value))])
@@ -50,14 +73,22 @@ function withBreakpoints<
     return active as TActiveBreakpoints<T>;
   }
 
-  return stateful(calculate, (state) => {
+  const get: () => TActiveBreakpoints<T> | U = map
+    ? () => map(calculate())
+    : calculate;
+
+  return stateful(get(), (state) => {
     let timeout: NodeJS.Timer | null = null;
     const listener = (): void => {
       if (timeout) clearTimeout(timeout);
       timeout = setTimeout(() => {
-        const active = calculate();
-        if (active !== state.current) state.set(active);
-      }, 100);
+        const value = get();
+        if (typeof value === 'object' && value !== null) {
+          if (!equal(value, state.current)) state.set(value);
+        } else {
+          if (value !== state.current) state.set(value);
+        }
+      }, 150);
     };
 
     w.addEventListener('resize', listener);
