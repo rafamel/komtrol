1e6d5236d5b4e046dd87b4a8faef326167ea3eb0
Rafa Mel <melrcm@gmx.net>
2019-07-11 07:07:55 +0200

    feat(withFields): adds withFields

--- src/fns/index.ts
+++ src/fns/index.ts
@@ -1,3 +1,4 @@
 export { default as withState } from './state';
 export { default as withStates } from './states';
 export { default as withField } from './field';
+export { default as withFields } from './fields';
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 export { default as pipe } from './pipe';
 export { default as fu } from './fu';
 export { default as create } from './create';
+export { default as lift } from './lift';
 export * from './fns';
 export * from './utils';
 export * from './types';
