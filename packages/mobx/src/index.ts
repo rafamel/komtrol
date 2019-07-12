f774f5d50415fa27251e0355d8c28cc96ba2480d
Rafa Mel <melrcm@gmx.net>
2019-07-12 21:47:57 +0200

    feat(mobx): withMobx can take an update policy param

--- packages/mobx/src/index.ts
+++ packages/mobx/src/index.ts
@@ -1,31 +1,86 @@
-import { stateful, mapTo, TFu } from 'komfu';
+import {
+  mapTo,
+  TFu,
+  TUpdatePolicy,
+  fu,
+  initializePolicy,
+  connect,
+  lift
+} from 'komfu';
 import { toStream } from 'mobx-utils';
+import { BehaviorSubject, combineLatest } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+export type TMobxCompute<A, B> = (self: A) => B;
 
 export default withMobx;
 
 function withMobx<A extends object, B extends object>(
-  fn: () => B
+  compute: TMobxCompute<A, B>
+): TFu<A, A & B>;
+function withMobx<A extends object, B extends object>(
+  policy: TUpdatePolicy<A>,
+  compute: TMobxCompute<A, B>
 ): TFu<A, A & B>;
 function withMobx<A extends object, B, K extends string>(
   key: K,
-  fn: () => B
+  compute: TMobxCompute<A, B>
+): TFu<A, A & { [P in K]: B }>;
+function withMobx<A extends object, B, K extends string>(
+  key: K,
+  policy: TUpdatePolicy<A>,
+  compute: TMobxCompute<A, B>
 ): TFu<A, A & { [P in K]: B }>;
 
 function withMobx<A extends object, B, K extends string>(
-  a: K | (() => B),
-  b?: () => B
+  a: TMobxCompute<A, B> | TUpdatePolicy<A> | K,
+  b?: TMobxCompute<A, B> | TUpdatePolicy<A>,
+  c?: TMobxCompute<A, B>
 ): TFu<A, A & (B | { [P in K]: B })> {
   const hasKey = typeof a === 'string';
+  const hasPolicy = hasKey ? c !== undefined : b !== undefined;
   const key = hasKey ? (a as K) : null;
-  const fn = (hasKey ? b : a) as (() => B);
-  const mapper = mapTo<A, B, K>(key);
+  const policy = (hasPolicy ? (hasKey ? b : a) : false) as TUpdatePolicy<A>;
+  const compute = (hasKey
+    ? hasPolicy
+      ? c
+      : b
+    : hasPolicy
+    ? b
+    : a) as TMobxCompute<A, B>;
+  const mapper = mapTo(key);
 
-  return stateful(fn, (stateful) => {
-    const stream = toStream(fn);
-    const subscription = stream.subscribe((value) => stateful.set(value));
-    return {
-      map: mapper,
-      teardown: subscription.unsubscribe
-    };
+  const wrap = policy ? connect : lift;
+
+  return wrap((self) => {
+    return fu((instance) => {
+      let lastMobx = false;
+
+      const stream = toStream(() => {
+        lastMobx = true;
+        return compute(self);
+      });
+      const enactPolicy = initializePolicy(policy, (self) => {
+        lastMobx = false;
+        return compute(self);
+      });
+
+      const subject = new BehaviorSubject(enactPolicy(instance.initial));
+      const subscription = stream.subscribe((value) => subject.next(value));
+      return {
+        initial: mapper(instance.initial, subject.value),
+        subscriber: policy
+          ? combineLatest(
+              instance.subscriber.pipe(map((a): [A, B] => [a, enactPolicy(a)])),
+              subject
+            ).pipe(
+              map(([[a, bSync], bMobx]) => mapper(a, lastMobx ? bMobx : bSync))
+            )
+          : combineLatest(instance.subscriber, subject).pipe(
+              map(([a, b]) => mapper(a, b))
+            ),
+        teardown: subscription.unsubscribe
+      };
+    });
   });
 }
