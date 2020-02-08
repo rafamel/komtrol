5a41ff8039a433d8245d47f6b501c36a1cdcc4aa
Rafa Mel <melrcm@gmx.net>
2020-02-08 14:24:48 +0100

    feat: adds Controller

---
+++ src/Controller.ts
@@ -0,0 +1,19 @@
+import { PublicStore } from './sources';
+
+export abstract class Controller<S = any, C = any, D = void> {
+  private store: PublicStore<S, C>;
+  protected dependencies: D;
+  public constructor(store: PublicStore<S, C>, dependencies: D) {
+    this.store = store;
+    this.dependencies = dependencies;
+  }
+  protected get context(): C {
+    return this.store.context;
+  }
+  protected get state(): S {
+    return this.store.data();
+  }
+  protected get next(): PublicStore<S>['next'] {
+    return this.store.next.bind(this.store);
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export * from './sources';
+export * from './Controller';
 export * from './utils';
 
 // possible names: storio, gaio, gaion, faion, komjet, komstore, komiet, komtrol, komst, komio, komore, komtash
