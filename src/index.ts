973075a922e649782fcbfad75f3c078c2f9661c0
Rafa Mel <melrcm@gmx.net>
2020-02-08 14:07:28 +0100

    chore: removes source files for rewrite

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,2 @@
-export * from './Controller';
-export * from './Store';
-export * from './helpers';
-export * from './react';
-
 // possible names: storio, gaio, gaion, faion, komjet, komstore, komiet, komtrol, komst, komio, komore, komtash
 // komtrol, komio, komore, komtash
