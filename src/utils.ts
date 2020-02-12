f6943969b784b109953068f0cc2e73b8e1fec34d
Rafa Mel <melrcm@gmx.net>
2020-02-12 13:06:36 +0100

    feat: adds match util

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,6 @@
 import { shallowEqual as shallow } from 'shallow-equal-object';
+import { Source } from './sources';
+import { Observable, merge, of } from 'rxjs';
 
 export function compute<I, O>(deps: () => I, fn: (deps: I) => O): () => O {
   let lastDependencies: null | I = null;
@@ -20,4 +22,60 @@ export function compute<I, O>(deps: () => I, fn: (deps: I) => O): () => O {
 
     return lastResult;
   };
+}
+
+export function match<T>(
+  source: Source<T>,
+  state: Partial<T>,
+  debounce?: number
+): Observable<boolean> {
+  const keys: null | Array<keyof T> =
+    typeof state === 'object' && state !== null
+      ? (Object.keys(state) as Array<keyof T>)
+      : null;
+
+  const isMatch = (value: T): boolean => {
+    if (!keys) return value === state;
+
+    for (const key of keys) {
+      if (state[key] !== value[key]) return false;
+    }
+    return true;
+  };
+
+  return new Observable<boolean>((obs) => {
+    let last: null | boolean = null;
+    let next: null | boolean = null;
+    let timeout: null | NodeJS.Timeout = null;
+
+    const subscription = merge(of(source.state), source.state$).subscribe({
+      next(state) {
+        const value = isMatch(state);
+
+        if (!debounce && debounce !== 0) {
+          if (value === last) return;
+          last = value;
+          return obs.next(value);
+        } else {
+          if (value === next) return;
+          if (timeout) clearTimeout(timeout);
+          if (value === last) return;
+
+          next = value;
+          timeout = setTimeout(() => {
+            next = null;
+            last = value;
+            obs.next(value);
+          }, debounce);
+        }
+      },
+      error: (err) => obs.error(err),
+      complete: () => obs.complete()
+    });
+
+    return () => {
+      subscription.unsubscribe();
+      if (timeout) clearTimeout(timeout);
+    };
+  });
 }
