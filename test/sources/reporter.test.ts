0f0e5ffaf0d8198834e74fd366398cc32fef5828
Rafa Mel <melrcm@gmx.net>
2020-03-02 00:12:12 +0100

    test(sources): adds tests for Reporter

---
+++ test/sources/reporter.test.ts
@@ -0,0 +1,37 @@
+import { subscribe } from 'promist';
+import { Enclosure, ReporterResource, ReporterSubject } from '~/sources';
+
+const mocks = {
+  next: jest.spyOn(Enclosure.prototype as any, 'next'),
+  report: jest.spyOn(ReporterResource.prototype as any, 'report')
+};
+
+test(`methods call super`, () => {
+  Object.values(mocks).map((mock) => mock.mockClear());
+  const subject = new ReporterSubject(null, null);
+
+  expect(mocks.next).toHaveBeenCalledTimes(0);
+  expect(mocks.report).toHaveBeenCalledTimes(0);
+
+  subject.next(null, true);
+  expect(mocks.next).toHaveBeenLastCalledWith(null, true);
+
+  const err = Error('foo');
+  subject.report(err);
+  expect(mocks.report).toHaveBeenLastCalledWith(err);
+});
+test(`error$ emits on report`, async () => {
+  const subject = new ReporterSubject(null, null);
+  const err = Error(`foo`);
+  const errors: Error[] = [];
+  const promise = subscribe(subject.error$);
+  const subscription = subject.error$.subscribe((err) => errors.push(err));
+
+  subject.report(err);
+  await expect(promise).resolves.toBe(err);
+
+  subject.report(Error());
+  subject.report(Error());
+  subscription.unsubscribe();
+  expect(errors).toHaveLength(3);
+});
