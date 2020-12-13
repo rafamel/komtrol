23659cda83ef26c1f1f69d985ae4c04debd4c3bf
Rafa Mel <melrcm@gmx.net>
2020-12-13 12:11:32 +0100

    improvement(utils): implements states and states$ utilities as Combine static methods; removes compu
    BREAKING CHANGE: Combination utility functions states() and states$() are now Combine.states() and
    Combine.states$(); the compute() utility function has been removed

---
+++ src/utils/combine.ts
@@ -0,0 +1,31 @@
+import { Source } from '../definitions';
+import { Members } from 'type-core';
+import { Push } from 'multitude/definitions';
+import { combine } from 'multitude/push';
+
+export declare namespace Combine {
+  export type States<T extends Members<Source>> = {
+    [P in keyof T]: T[P]['state'];
+  };
+}
+
+export class Combine {
+  public static states<T extends Members<Source>>(
+    sources: T
+  ): Combine.States<T> {
+    return Object.entries(sources).reduce((acc: any, [key, value]) => {
+      acc[key] = value.state;
+      return acc;
+    }, {});
+  }
+  public static states$<T extends Members<Source>>(
+    sources: T
+  ): Push.Observable<Combine.States<T>> {
+    const states = Object.entries(sources).reduce((acc: any, [key, value]) => {
+      acc[key] = value.state$;
+      return acc;
+    }, {});
+
+    return combine(states) as Push.Observable<Combine.States<T>>;
+  }
+}
